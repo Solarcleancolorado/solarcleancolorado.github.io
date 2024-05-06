dd8b05f0b8219fb5679a8604b24812bb9f4b880e
Samuel Krohn <sanikr@outlook.com>
2024-05-05 20:11:05 -0600

    Update copyright

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,13 +1,13 @@
 import * as React from "react"
 import "./Footer.scss"
-import { FaCopyright } from "@react-icons/all-files/fa/FaCopyright";
+import { FaRegCopyright  } from "@react-icons/all-files/fa/FaRegCopyright";
 
 const Footer = () => {
   return (
     <footer class="footer navbar navbar-dark bg-dark">
         <div class="container-fluid">
             <span class="navbar-brand mb-0">
-                <FaCopyright />&nbsp;<em>Copyright 2024 Solar Clean Colorado, LLC.</em>
+                <FaRegCopyright />&nbsp;<em>Copyright 2024 Solar Clean Colorado</em>
             </span>
         </div>
     </footer>
