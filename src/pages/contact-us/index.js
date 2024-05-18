0ef933b9d31f52bc0ec9a0aa7f970e3d7d4f2af8
Samuel Krohn <sanikr@outlook.com>
2024-05-18 09:54:09 -0600

    Update contact us page

--- src/pages/contact-us/index.js
+++ src/pages/contact-us/index.js
@@ -6,6 +6,8 @@ import { FaPhoneAlt  } from "@react-icons/all-files/fa/FaPhoneAlt";
 import { FaEnvelope } from "@react-icons/all-files/fa/FaEnvelope";
 import BorderBreak from "../../components/BorderBreak/BorderBreak"
 import { headData } from "../../head"
+import { FaFacebookSquare } from "@react-icons/all-files/fa/FaFacebookSquare";
+import { SiNextdoor } from "@react-icons/all-files/si/SiNextdoor";
 
 const IndexPage = () => {
   return (
@@ -23,9 +25,19 @@ const IndexPage = () => {
                 <p>Monday - Friday<br />9:00 AM - 5:00 PM</p>
                 <p>Saturday<br />9:00 AM - 1:00 PM</p>
               </div>
-              <div className="contact-details">
-                <span className="d-flex align-items-center"><FaPhoneAlt />&nbsp;<a href="tel:720-541-3606">(720) 541-3606</a></span>
-                <span className="d-flex align-items-center"><FaEnvelope />&nbsp;<a href="mailto:info@solarcleancolorado.com">info@solarcleancolorado.com</a></span>
+              <div className="contact-and-social">
+                <div className="contact-details">
+                  <h3 className="text-start">Call, Text or Email Us</h3>
+                  <span className="d-flex align-items-center"><FaPhoneAlt />&nbsp;<a href="tel:720-541-3606">(720) 541-3606</a></span>
+                  <span className="d-flex align-items-center"><FaEnvelope />&nbsp;<a href="mailto:info@solarcleancolorado.com">info@solarcleancolorado.com</a></span>
+                </div>
+                <div>
+                  <h3 className="text-start">Follow Our Social Media</h3>
+                  <div className="social">
+                    <a href="https://www.facebook.com/people/Solar-Clean-Colorado/61559218996878/" target="_blank" rel="noopener noreferrer" aria-label="Facebook Link"><FaFacebookSquare /></a>
+                    <a href="https://nextdoor.com/pages/solar-clean-colorado-castle-rock-co/?query=solar%20clean%20colorado" target="_blank" rel="noopener noreferrer" aria-label="Nextdoor Link"><SiNextdoor /></a>
+                  </div>        
+                </div>
               </div>
             </div>
             <p>At Solar Clean Colorado, we specialize in residential and commercial window and solar panel cleaning. Our dedicated team ensures your property shines and operates efficiently. With meticulous attention to detail and professional service, we guarantee to exceed your expectations.</p>
