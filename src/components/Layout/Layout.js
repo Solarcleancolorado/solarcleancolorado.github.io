ce80f164c0e2a94e2943a156d5dba1c577085f45
Samuel Krohn <SaNiKr@outlook.com>
2025-03-01 23:50:43 +0000

    Update Layout.js

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -6,30 +6,8 @@ import Footer from "./components/Footer/Footer"
 import "./Layout.scss"
 
 const Layout = ({ children }) => {
-
-  const data = useStaticQuery(graphql`
-    query {
-      allMarkdownRemark(filter: {parent: {internal: {description: {regex: "/popup/"}}}}) {
-        nodes {
-          frontmatter {
-            title
-            message
-            enabled
-          }
-        }
-      }
-    }
-  `);
-
-  const popup = data.allMarkdownRemark.nodes[0]?.frontmatter;
-
   return (
     <>
-    {popup.enabled && 
-    (<div className="popup">
-      <h2>{popup.title}</h2>
-      <p>{popup.message}</p>
-    </div>)}
       <Header></Header>
       <Navbar />
         <main className="body">
